87f9641d18663ca4c2da63b95111b3ad22e2435a
Sergio Martín <sergio@sergiomartin.dev>
2022-02-11 10:18:21 +0100

    add source-sans font

--- src/App.vue
+++ src/App.vue
@@ -15,10 +15,11 @@ export default {
 </script>
 
 <style>
+:root {
+  font-family: 'Source Sans 3 VF', sans-serif;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
+import 'source-sans/source-sans-3VF.css'
+
 createApp(App).mount('#app');
