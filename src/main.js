73be59a5ca4156f925feefcb8e50abb1524b9837
Sergio Martín <sergio@sergiomartin.dev>
2022-02-11 10:22:18 +0100

    add remixicon icons

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
-import 'source-sans/source-sans-3VF.css'
+import 'source-sans/source-sans-3VF.css';
+import 'remixicon/fonts/remixicon.css';
 
 createApp(App).mount('#app');
